e7cde6f6f8688cb204ef336fcd938c357612c90a
Andres Hugueth <andreshugueth@gmail.com>
2025-09-16 01:03:46 -0500

    Test: Add test database and separate containers by profiles

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,33 @@
 import pytest
 from app.api import app
 from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from app.infrastructure.models.base import Base
+
+TEST_DB_URL = "postgresql+asyncpg://test_user:test_password@localhost:5433/test_db"
+
+engine = create_async_engine(TEST_DB_URL)
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+@pytest.fixture(scope="session")
+async def setup_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+@pytest.fixture(scope="session")
+async def session(setup_database):
+    async with SessionLocal() as session:
+        yield session
+
 
 @pytest.fixture(scope="session")
 async def test_app():
